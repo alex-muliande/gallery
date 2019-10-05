b32440cb2f69bba2211c1284e05d76c1edafd867
alex-muliande <amuliande@gmail.com>
2019-10-05 17:45:27 +0300

    Add changes to the models, add  class location.

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Location(models.Model):
+    location = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.location
+
+    class Meta:
+        odering = ['location']
+
+    def save_location(self):
+        self.save()
+
+    
--- photos/tests.py
+++ photos/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from .models import Location
 
 # Create your tests here.
+class Location(TestCase):
+
+    #set up method
+    def setUp(self):
+        self.nairobi= Location(location='nairobi')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.nairobi,Location))
+
+    def tearDown(self):
+        Location.objects.all()
+
+    def test_save_method(self):
+        self.nairobi.save_location()
+        locations = Location.objects.all()
+        self.assertTrue(len(location)>0)
+
