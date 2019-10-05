a47cc33248a668936481325641fe09bd100c3ad3
alex-muliande <amuliande@gmail.com>
2019-10-05 19:08:24 +0300

    Add class Category this commit will make the photos to have different categories.

--- photos/models.py
+++ photos/models.py
@@ -13,4 +13,11 @@ class Location(models.Model):
     def save_location(self):
         self.save()
 
+class Category(models.Model):
+    category = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.category
+
+
     
