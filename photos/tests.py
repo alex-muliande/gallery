11cb77d0774f1293f341baba93c97e08062576a4
alex-muliande <amuliande@gmail.com>
2019-10-06 08:26:19 +0300

    Add tests for saving an image and deleting an Image

--- photos/tests.py
+++ photos/tests.py
@@ -1,8 +1,8 @@
 from django.test import TestCase
-from .models import Location
+from .models import Location,Image,Category
 
 # Create your tests here.
-class Location(TestCase):
+class LocationTestClass(TestCase):
 
     #set up method
     def setUp(self):
@@ -12,10 +12,16 @@ class Location(TestCase):
         self.assertTrue(isinstance(self.nairobi,Location))
 
     def tearDown(self):
-        Location.objects.all()
+        Location.objects.all().delete()
 
     def test_save_method(self):
         self.nairobi.save_location()
         locations = Location.objects.all()
-        self.assertTrue(len(location)>0)
+        self.assertTrue(len(locations)>0)
+
+    def test_delete_method(self):
+        self.nairobi.delete_location('nairobi')
+        locations = Location.objects.all()
+        self.assertTrue(len(locations)==0)
+    
 
