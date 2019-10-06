4c1baaa11150ebaf819b86bf8bd74eb77fdd8abe
alex-muliande <amuliande@gmail.com>
2019-10-06 08:25:24 +0300

    Ass delete function

--- photos/models.py
+++ photos/models.py
@@ -7,17 +7,25 @@ class Location(models.Model):
     def __str__(self):
         return self.location
 
-    class Meta:
-        odering = ['location']
 
     def save_location(self):
         self.save()
+
+    def delete_location(self):
+        self.delete()
 
 class Category(models.Model):
     category = models.CharField(max_length=30)
 
     def __str__(self):
         return self.category
+
+class Image(models.Model):
+    title =models.CharField(max_length =60)
+    post_date = models.DateTimeField(auto_now_add=True)
+    location= models.ForeignKey(Location)
+    category= models.ForeignKey(Category)
+    image = models.ImageField(upload_to ='images/' ,default='DEFAULT VALUE')
 
 
     
